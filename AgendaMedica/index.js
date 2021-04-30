23310a9d0cf741681f15c1c4822d59b64670219f
Felipe <felipe.falcao@dcomp.ufs.br>
2021-04-30 19:09:24 -0300

    adding rules

--- AgendaMedica/index.js
+++ AgendaMedica/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import Navigator from './src/Navigator';
+import {AppRegistry} from 'react-native'
+import {name as appName} from './app.json'
+import Navigator from './src/Navigator'
 
 AppRegistry.registerComponent(appName, () => Navigator);
